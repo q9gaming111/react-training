244ecadea015480816edd0c21cedecd2fc1fdb89
Tu Nguyen <tu.nguyen@dfo.global>
2022-06-29 14:44:23 +0700

    update miragejs server

---
+++ mirage-tutorial/src/server.js
@@ -0,0 +1,27 @@
+import { createServer, Model } from "miragejs"
+
+export default function () {
+  createServer({
+    models: {
+      reminder: Model,
+    },
+
+    seeds(server) {
+      server.create("reminder", { text: "Walk the dog" })
+      server.create("reminder", { text: "Take out the trash" })
+      server.create("reminder", { text: "Work out" })
+    },
+
+    routes() {
+      this.get("/api/reminders", (schema) => {
+        return schema.reminders.all()
+      })
+
+      this.post("/api/reminders", (schema, request) => {
+        let attrs = JSON.parse(request.requestBody)
+
+        return schema.reminders.create(attrs)
+      })
+    },
+  })
+}

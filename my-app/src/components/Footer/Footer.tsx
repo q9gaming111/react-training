268c004084164406abf12b4b5417a2efe354569c
Tu Nguyen <tu.nguyen@dfo.global>
2022-08-16 17:12:05 +0700

    update

--- my-app/src/components/Footer/Footer.tsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <div>
-      <header>Footer</header>
-    </div>
-  )
-}
--- my-app/src/components/Header/Header.tsx
+++
@@ -1,7 +0,0 @@
-export default function Header() {
-  return (
-    <div>
-      <header>Header</header>
-    </div>
-  )
-}
